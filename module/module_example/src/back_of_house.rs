bd2293c110a999cac9df5b84eeaf5b9a8cf0917c
Shangjin Tang <shangjin.tang@gmail.com>
2023-10-03 03:45:45 +0800

    add module/module_example

---
+++ module/module_example/src/back_of_house.rs
@@ -0,0 +1,10 @@
+#![allow(unused)]
+#![allow(dead_code)]
+
+use crate::front_of_house;
+pub fn fix_incorrect_order() {
+    cook_order();
+    front_of_house::serving::serve_order();
+}
+
+pub fn cook_order() {}
---
+++ module/module_example/src/front_of_house/serving.rs
@@ -0,0 +1,11 @@
+#![allow(dead_code)]
+
+pub fn take_order() {}
+
+pub fn serve_order() {}
+
+pub fn take_payment() {}
+
+// Maybe you don't want the guest hearing the your complaining about them
+// So just make it private
+fn complain() {}
